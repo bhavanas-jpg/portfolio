ce9c9e37c561bb223a85d6ae34ec62044fcd71c2
pavan6cs <bhavanas904@gmail.com>
2022-07-07 14:25:46 +0530

    added images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './portfolio.css';
-import IMG1 from '../../assets/portfolio1.jpg';
-import IMG2 from '../../assets/portfolio2.jpg';
-import IMG3 from '../../assets/portfolio3.jpg';
-import IMG4 from '../../assets/portfolio4.jpg';
+import IMG1 from '../../assets/project1.jpg';
+import IMG2 from '../../assets/project2.jpg';
+import IMG3 from '../../assets/project3.jpg';
+import IMG4 from '../../assets/project4.jpg';
 import IMG5 from '../../assets/portfolio5.png';
 import IMG6 from '../../assets/portfolio6.jpg';
 
@@ -11,27 +11,32 @@ import IMG6 from '../../assets/portfolio6.jpg';
 const data = [
   {
     id: 1,
-    image: IMG1,
+    image: IMG3,
     title: 'Fraser Property',
-    github: 'https://github.com',
-    demo: ''
+    github: '',
+    demo: 'https://www.frasersproperty.com/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Fraser Property',
-    github: 'https://github.com',
-    demo: ''
-  },{
+    title: 'Johnson Matthey',
+    github: '',
+    demo: 'https://matthey.com/products-and-markets/pgms-and-circularity/pgm-management'
+  },
+  {
     id: 3,
-    image: IMG3,
-    title: 'Fraser Property',
-    github: 'https://github.com',
-    demo: ''
+    image: IMG4,
+    title: 'LinkedIn Clone',
+    github: 'https://github.com/bhavanas-jpg/LinkedIn_clone/tree/master/linkedin',
+    demo: 'https://linkedin-clone-c2e52.web.app'
   }
+  
 ]
 
 const Portfolio = () => {
+
+  const[githubState, setGithubState]=useState('');
+
   return (
     <section id="projects">
        <h5>My Recent Work</h5>
@@ -40,15 +45,23 @@ const Portfolio = () => {
     <div className="container portfolio__container">
     {
       data.map(({id, image, title, github, demo}) =>{
+          if(!githubState){
+            setGithubState(github)
+          }
+
         return(
           <article key={id}className="portfolio__item">
           <div className="portfolio__item-image">
-           <img src={image} alt={title}/>
+           <img src={image} alt={title} className="portfolio__image"/>
           </div>
           <h3>{title}</h3>
           <div className="portfolio__item-cta">
-          <a href={github} className="btn" target="_blank">Github</a>
-          <a href={demo} className="btn btn-primary" target="_blank">Live Demo</a>
+            {
+             github ?
+               ( <a href={github} className="btn" target="_blank">Github</a>)
+               : null
+              }
+          <a href={demo}  className="btn btn-primary" target="_blank">Live Demo</a>
           </div>     
           </article>
         )
